68933a4898db53dd637450937f63fa06e265d27f
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-08-30 21:58:39 +0600

    profile models create

---
+++ models/Profile.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+// - First Name
+// - Last Name
+// - Phone No
+// - Profile Picture
+// - UserId
+
+const { model, Schema } = mongoose;
+
+const profileSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  phoneNo: String,
+  avatar: String,
+  User: {
+    type: Schema.type.ObjectId,
+    ref: "User",
+  },
+});
+const Profile = model("Profile", profileSchema);
+
+module.exports = Profile;
