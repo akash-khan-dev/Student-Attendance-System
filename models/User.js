2f3c1ae6b41692bfc56318fa565c282713cc1a1c
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-09-16 02:39:36 +0600

    Real User model complet

--- models/User.js
+++ models/User.js
@@ -9,11 +9,40 @@ const mongoose = require("mongoose");
 const { model, Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  roles: [String],
-  accountStatus: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 10,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          v
+        );
+      },
+      message: (prop) => `Invalide Emalil${prop.value}`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Min 6 charectior"],
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: ["STUDENT"],
+  },
+  accountStatus: {
+    type: String,
+    enum: ["PENDING", "ACTIVE", "REJECTED"],
+    default: "PENDING",
+    required: true,
+  },
 });
 
 const User = model("User", UserSchema);
