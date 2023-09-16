3ffa691c570ba9d55cb415fccfdc6c6aad139e83
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-09-16 14:15:09 +0600

    login processe complet

--- server.js
+++ server.js
@@ -28,31 +28,77 @@ app.post("/register", async (req, res) => {
    -req Header
    -req Cookies
    */
+  // user information
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json({ message: "please full fill the data" });
   }
+  try {
+    // find email like new email axist in before database
+    let user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(400).json({ message: "User already axist" });
+    }
+    //new user create
+    user = new User({ name, email, password });
+    //password hashing
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  let user = await User.findOne({ email: email });
-  if (user) {
-    return res.status(400).json({ message: "User already axist" });
+    user.password = hash;
+    user.save();
+    return res.status(201).json({ message: "user created Successfully", user });
+  } catch (e) {
+    next(e);
   }
-  user = new User({ name, email, password });
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-  user.password = hash;
-  user.save();
-  return res.status(201).json({ message: "user created Successfully", user });
-});
-app.get("/", (req, res) => {
-  const obj = {
-    name: "akash",
-    email: "akash@exmple.com",
-  };
-  res.json(obj);
 });
 
+/**
+ * **start**
+email = input()
+password= input ()
+user = find  user with email
+if user not found:
+return 400 error
+if password not equal to user.hash
+return 400 error
+token  = generate token using user
+return token
+End
+ */
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // email validation
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User Credential" });
+    }
+    // password validation
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "User Credential" });
+    }
+    return res.status(200).json({ message: "Login Successful" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// global error handle
+app.use((err, req, res, nex) => {
+  console.log(err);
+  res.status(500).json({ message: "Server Error Occure" });
+});
+// app.get("/", (req, res) => {
+//   const obj = {
+//     name: "akash",
+//     email: "akash@exmple.com",
+//   };
+//   res.json(obj);
+// });
+
+// mongoose connection
 connectDb("mongodb://127.0.0.1:27017/AttendaceSystem")
   .then(() => {
     console.log("database connected");
