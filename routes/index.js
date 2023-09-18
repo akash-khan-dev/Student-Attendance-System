9997858db4ca39371ec26d1c4ff593488281f515
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-09-19 01:05:47 +0600

    all user getting authorization

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const authRoute = require("./auth");
 const userRouters = require("./users");
+const authenticate = require("../Middelware/authentication");
 
+router.use("/api/v1/users", authenticate, userRouters);
 router.use("/api/v1/auth", authRoute);
-router.use("/api/v1/users", userRouters);
 
 module.exports = router;
